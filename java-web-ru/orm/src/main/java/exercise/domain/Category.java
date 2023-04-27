5a375b9c92b9780a3d433ac3a76114feadcc5192
@hexlet/cli <support@hexlet.io>
2023-04-27 15:17:57 +0600

    submit java-web-ru/orm

---
+++ java-web-ru/orm/src/main/java/exercise/domain/Category.java
@@ -0,0 +1,45 @@
+package exercise.domain;
+
+import io.ebean.Model;
+import io.ebean.annotation.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+import javax.persistence.CascadeType;
+
+// Указываем, что класс является моделью
+@Entity
+public class Category extends Model {
+
+    // Указываем, что поле является первичным ключом
+    // Значение будет генерироваться автоматически
+    @Id
+    private long id;
+
+    @NotNull
+    private String name;
+
+    // Задаём связь "один ко многим"
+    // Указываем тип данных Article для свойства
+    // Тем самым указываем связанную сущность
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Article> articles;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public List<Article> getArticles() {
+        return this.articles;
+    }
+}
